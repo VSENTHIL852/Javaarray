e4b1392a9add9cc309f36522675ec22de950551f
SENTHILKUMAR V <120891309+VSENTHIL852@users.noreply.github.com>
2025-03-12 09:07:10 +0530

    Create Arraysort.java

---
+++ Arraysort.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Arraysort{
+    public static void main(String[] args){
+        int[] arr1={10,9,8,7,6,5,4,2,3,1,0};
+        String[] arr2={"zero","word","one"};
+        System.out.println(arr1);
+        System.out.println(arr1);
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
+    }
+}
