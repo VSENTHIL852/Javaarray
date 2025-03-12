6cb70cd893d0a665cc08e0d50640d594d64b0a90
SENTHILKUMAR V <120891309+VSENTHIL852@users.noreply.github.com>
2025-03-12 09:09:10 +0530

    Create Arraymat.java

---
+++ Arraymat.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Arraymat{
+    public static void main(String[] args){
+        int[][] arr1=new int[10][10];
+        for(int i=0;i<10;i++)
+        {
+            for(int j=0;j<10;j++)
+            {
+                System.out.printf("%2d",arr1[i][j]);
+            }
+            
+        
+        System.out.println();
+        }
+    }
+}
