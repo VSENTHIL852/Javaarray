5aa45b3115f765aba06c00fb36580ecec950df02
SENTHILKUMAR V <120891309+VSENTHIL852@users.noreply.github.com>
2025-03-17 09:49:26 +0530

    Create Listex.java

---
+++ Listex.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+public class Listex{
+  public static void main(String[] args){
+    ArrayList<String> list=new ArrayList<>();
+    list.add("Red");
+    list.add("Green");
+    list.add("Orange");
+    list.add("White");
+    list.add("Black");
+    System.out.println(list);
+  }
+}
